7feb31b8be8478d08342339ee0a2cb3284cf5ec1
Pavomuticus <89340536+Pavomuticus@users.noreply.github.com>
2021-10-11 23:45:20 +0800

    Update clion-backspace.el

--- clion-backspace.el
+++ clion-backspace.el
@@ -2,22 +2,18 @@
   "Simulate clion's backspace key behavior"
   (interactive "*P")
   (if (and (<= (current-column) (current-indentation)) (not (use-region-p)))
-      (if (= (current-indentation) (c-get-syntactic-indentation (c-guess-basic-syntax)))
-	  (if (previous-line-blank-p)
-	      (progn (delete-region (line-beginning-position 0) (line-beginning-position)) (back-to-indentation))
-	    (delete-indentation))
-	(if (bolp)
-	    (if (previous-line-blank-p)
-		(progn (delete-indentation) (c-indent-line))
-	      (delete-indentation))
-	  (c-indent-line)))
+      (if (bolp)
+	  (progn (c-indent-line) (plb))
+	(when (= (c-indent-line) 0) (plb)))
     (c-electric-backspace arg)))
 
-(defun previous-line-blank-p ()
-  (save-excursion
-    (previous-line)
-    (beginning-of-line)
-    (looking-at "[[:space:]]*$")))
+(defun plb ()
+  (if (save-excursion
+	(previous-line)
+	(beginning-of-line)
+	(looking-at "[[:space:]]*$"))
+      (delete-region (line-beginning-position 0) (line-beginning-position))
+    (delete-indentation)))
 
 (define-key c-mode-map (kbd "DEL") 'clion-backspace)
 
