8184994bfec43c5e4ee2fb2fac129fcd3a9a151a
Pavomuticus <89340536+Pavomuticus@users.noreply.github.com>
2022-02-24 21:11:02 +0800

    Update clion-backspace.el

--- clion-backspace.el
+++ clion-backspace.el
@@ -16,9 +16,7 @@
       (null (delete-region
 	     (line-beginning-position 0)
 	     (line-beginning-position)))
-    (cl-letf* (((symbol-function 'fixup-whitespace)
-		#'delete-horizontal-space))
-	      (delete-indentation))))
+    (delete-indentation)))
 
 (define-key c-mode-map (kbd "DEL") 'clion-backspace)
 
